fc7933f0c6cab2f90ae18f6c346dd0cbe8e4e0b4
Andrey Galkin <agalkingps@yandex.ru>
2023-03-04 11:17:47 +0300

    SignIn and Login screens initial implemention

---
+++ app/src/main/java/ru/agalking/mykotlintestapp/views/login/LoginActivity.kt
@@ -0,0 +1,26 @@
+package ru.agalking.mykotlintestapp.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import ru.agalking.mykotlintestapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    fun onSignInFragmentLoginButtonClicked(view: View) {
+        navController.navigate(R.id.action_signInFragment_to_loginFragment)
+    }
+}
